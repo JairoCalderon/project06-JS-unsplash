ed6b8a39907572180ede114b013f6e8c16e99dc1
JairoCalderon <j.calderoneng@gmail.com>
2022-07-24 17:26:31 -0400

    finishing app

--- app.js
+++ app.js
@@ -7,26 +7,45 @@
 
 const galleryApp = {};
 
-galleryApp.APIurl = "https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY";
+galleryApp.APIurl = "https://api.unsplash.com/photos";
 galleryApp.APIkey = "eXlwp7WDZh1qmTbQ-IM--mg-GGbME4PnIWe4343AJsw";
+
+const ulElm = document.querySelector(".gallery");
 
 galleryApp.getPhotos = () => {
     const url = new URL(galleryApp.APIurl);
     url.search = new URLSearchParams({
         client_id: galleryApp.APIkey,
     });
+    // console.log(url);
+    fetch(url).then((res) => {
+        return res.json();
+    }).then((data) => {
 
-    console.log(url);
+        //call the display data f(x):
+        galleryApp.displayPhotos(data)
+    })
 }
 
+galleryApp.displayPhotos = (APIdata) => {
+    console.log(APIdata);
+    const ulElm = document.querySelector(".gallery");
 
+    APIdata.forEach(obj => {
+        const liElm = document.createElement("li");
+        const image = document.createElement("img");
+
+        image.src = obj.urls.regular;
+        image.alt = obj.alt_description;
+
+        liElm.appendChild(image);
+
+        ulElm.appendChild(liElm)
+    });
+}
 galleryApp.init = () => {
-    console.log("hello");
     galleryApp.getPhotos();
-
 }
-
 
 galleryApp.init();
-
 
